befc576892e450fe050400bd83203a82c9a468f1
suvofalcon <suvo.bhattacharya@gmail.com>
2019-08-29 22:10:09 +0530

    Variables, Data Types, Mutation and Type Coercion

--- _01_LanguageBasics/script.js
+++ _01_LanguageBasics/script.js
@@ -1,1 +1,34 @@
-console.log('Hello World!!!');
+/*******************************
+* Variables and Data types
+*/
+
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job, isMarried;
+job = 'teacher';
+isMarried = 'false';
+
+console.log(job);
+
+/*************************************
+* Variables mutation and type coercion
+*/
+
+console.log(firstName + ' ' + age)
+console.log(firstName + ' ' + 'is' + ' ' + age + ' years old. He is a ' + job +'. Is he Married? ' + isMarried)
+
+// variable mutation
+age = 'twenty eight';
+job = 'Driver';
+
+alert(firstName + ' ' + 'is' + ' ' + age + ' years old. He is a ' + job +'. Is he Married? ' + isMarried)
+
+lastName = prompt('What is his last name?')
+console.log(firstName + ' ' + lastName)
