ed1b1222c5712501e1b99c70b1e0d399f378bf8c
suvofalcon <suvo.bhattacharya@gmail.com>
2022-08-13 21:40:08 +0530

    JavaScript Fundamentals

---
+++ _01_Fundamentals-I/Coding_Challenge1.js
@@ -0,0 +1,36 @@
+/*****
+Mark and John are trying to compare their BMI (Body Mass Index), which is
+calculated using the formula:
+BMI = mass / height ** 2 = mass / (height * height) (mass in kg
+and height in meter).
+Your tasks:
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both
+versions)
+3. Create a Boolean variable 'markHigherBMI' containing information about
+whether Mark has a higher BMI than John.
+Test data:
+§ Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95
+m tall.
+§ Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76
+m tall.
+ */
+
+let heightMark = 1.69;
+let heightJohn = 1.95;
+let weightMark = 78;
+let weightJohn = 92;
+
+//heightMark = 1.88
+//heightJohn = 1.76
+//weightMark = 95
+//weightJohn = 85
+
+
+bmiMark = weightMark / heightMark ** 2;
+bmiJohn = weightJohn / heightJohn ** 2;
+console.log('BMI of Mark ' + bmiMark)
+console.log('BMI of John ' + bmiJohn)
+
+let markHigherBMI = bmiMark > bmiJohn
+console.log(markHigherBMI)
--- _01_Fundamentals-I/script.js
+++ _01_Fundamentals-I/script.js
@@ -1,6 +1,5 @@
 /*******************************
 * Variables and Data types
-*/
 
 var firstName = 'John';
 console.log(firstName);
@@ -16,10 +15,12 @@ job = 'teacher';
 isMarried = 'false';
 
 console.log(job);
+*/
+
 
 /*************************************
 * Variables mutation and type coercion
-*/
+
 
 console.log(firstName + ' ' + age)
 console.log(firstName + ' ' + 'is' + ' ' + age + ' years old. He is a ' + job + '. Is he Married? ' + isMarried)
@@ -28,12 +29,102 @@ console.log(firstName + ' ' + 'is' + ' ' + age + ' years old. He is a ' + job + 
 age = 'twenty eight';
 job = 'Driver';
 
-alert(firstName + ' ' + 'is' + ' ' + age + ' years old. He is a ' + job + '. Is he Married? ' + isMarried)
+//alert(firstName + ' ' + 'is' + ' ' + age + ' years old. He is a ' + job + '. Is he Married? ' + isMarried)
 
-lastName = prompt('What is his last name?')
-alert('His Name is ' + firstName + ' ' + lastName)
+//lastName = prompt('What is his last name?')
+//alert('His Name is ' + firstName + ' ' + lastName)
 console.log(firstName + ' ' + lastName)
 
 let js = 'amazing';
-if (js == 'amazing') alert('JavaScript is Fun!!')
+//if (js == 'amazing') alert('JavaScript is Fun!!')
 console.log(40 + 8 + 23 - 10);
+*/
+
+/*************************************
+* Data Types
+
+
+//Boolean
+let javaScriptisFun = true;
+console.log(javaScriptisFun);
+
+console.log(typeof (javaScriptisFun))
+console.log(typeof 21);
+console.log(typeof 'Subhankar')
+
+let myAge = 'twenty';
+console.log(typeof myAge);
+myAge = 20;
+console.log(typeof myAge);
+
+let year // empty variable will hold the value undefined and also type
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null); // type of null in JS is object
+*/
+
+/************************************
+ * Let Const and Var
+ 
+
+//let is used to declare variable, which will undergo reassignment
+let age = 30
+age = 31;
+
+// const for values which will not change
+const birthYear = 1991;
+// birthYear = 1990 - Not legal
+
+var job = 'Programmer' //var is the old way of declaring variables in javsscript before ES6, not to be done now
+*/
+
+/***********************
+ * Operators - All arithmetic Operator
+ * + can be also used as string concatenate operator
+ * = assignment operator
+ */
+const now = 2020;
+const ageJonas = now - 2017;
+const ageLara = now - 1976;
+console.log(ageJonas, ageLara);
+
+console.log(ageJonas * 2, ageLara / 10, 2 ** 3);
+console.log((now - 2020) < (now - 2019));
+
+let x, y;
+x = y = 25 - 10 - 5;
+console.log(x, y)
+
+/**
+ * Strings and template literals
+ */
+
+firstName = 'Mark'
+age = 20
+const jonasNew = `I'm ${firstName} and i am ${age * 2} years old`
+console.log(jonasNew)
+
+console.log('String with \n\
+multiple \n\
+lines')
+
+console.log(`Another
+string
+with 
+multiple 
+lines`)
+
+/**
+ * Loops
+ */
+
+const permitAge = 15
+const isOldEnough = permitAge >= 18
+
+if (isOldEnough) {
+    console.log(`Can get Dring License`)
+} else {
+    const yearsLeft = 18 - permitAge
+    console.log(`Need to wait another ${yearsLeft} years`)
+}
